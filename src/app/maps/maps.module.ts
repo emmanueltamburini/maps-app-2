8b4ca1e832728d33a67c4fe788c922b00c1cae13
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-11-15 16:19:58 -0400

    my search components without functional part has been added

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -5,6 +5,8 @@ import { MapViewComponent } from './components/map-view/map-view.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { BtnMyLocationComponent } from './components/btn-my-location/btn-my-location.component';
 import { AngularLogoComponent } from './components/angular-logo/angular-logo.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SearchResultComponent } from './components/search-result/search-result.component';
 
 
 
@@ -14,7 +16,9 @@ import { AngularLogoComponent } from './components/angular-logo/angular-logo.com
     MapViewComponent,
     LoadingComponent,
     BtnMyLocationComponent,
-    AngularLogoComponent
+    AngularLogoComponent,
+    SearchBarComponent,
+    SearchResultComponent
   ],
   imports: [
     CommonModule
